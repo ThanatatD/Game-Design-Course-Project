172a18da2570861aad481f69305ec30d2c6a564f
ThanatatD <Thanatat5555@gmail.com>
2025-11-06 17:43:36 +0700

    add more on level 2

--- Assets/GameFolders/Scripts/Concretes/Controllers/LeverController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/LeverController.cs
@@ -1,3 +1,75 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Managers;
+
+// namespace Controllers
+// {
+//     public class LeverController : MonoBehaviour
+//     {
+//         [SerializeField] DoorController _door;
+//         [SerializeField] GameObject _checkMark;
+//         [SerializeField] GameObject _leverFruits;
+//         Animator _anim;
+//         bool IsLeverOn;
+//         bool CanLeverWork;
+//         private void Awake()
+//         {
+//             _anim = GetComponent<Animator>();
+//         }
+//         public void LeverInteraction()
+//         {
+//             if (!CanLeverWork)
+//             {
+//                 TryActivateLever();
+                
+//             }
+//             else
+//                 TriggerLever();
+//         }
+//         private void TryActivateLever()
+//         {
+//             if (CheckConditions())
+//             {
+//                 CanLeverWork = true;
+//                 FruitManager.Instance.DecreaseFruitNumber(_door.DoorFruitType, _door.DoorFruitNumber);
+//                 TriggerLever();
+//                 _checkMark.SetActive(true); 
+//                 if(_leverFruits != null) _leverFruits.SetActive(false);
+//             }
+//             else
+//                 SoundManager.Instance.PlaySound(7);
+//         }
+//         private void TriggerLever()
+//         {
+//             SoundManager.Instance.PlaySound(6);
+//             if (IsLeverOn)
+//                 LeverOff();
+//             else
+//                 LeverOn();
+
+//         }
+//         private void LeverOn()
+//         {
+//             IsLeverOn = true;
+//             _anim.SetBool("IsActive", true);
+//             _door.OpenDoor();
+//         }
+//         private void LeverOff()
+//         {
+//             IsLeverOn = false;
+//             _anim.SetBool("IsActive", false);
+//             _door.CloseDoor();
+//         }
+//         private bool CheckConditions()
+//         {
+//             return FruitManager.Instance.AreThereEnoughFruit(_door.DoorFruitType, _door.DoorFruitNumber);
+//         }
+//     }
+
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,64 +79,104 @@ namespace Controllers
 {
     public class LeverController : MonoBehaviour
     {
-        [SerializeField] DoorController _door;
-        [SerializeField] GameObject _checkMark;
-        [SerializeField] GameObject _leverFruits;
-        Animator _anim;
-        bool IsLeverOn;
-        bool CanLeverWork;
+        [Header("Doors Controlled by This Lever")]
+        [SerializeField] private List<DoorController> _doors = new List<DoorController>();
+
+        [Header("Lever Visuals")]
+        [SerializeField] private GameObject _checkMark;
+        [SerializeField] private GameObject _leverFruits;
+
+        private Animator _anim;
+        private bool IsLeverOn;
+        private bool CanLeverWork;
+
         private void Awake()
         {
             _anim = GetComponent<Animator>();
         }
+
         public void LeverInteraction()
         {
             if (!CanLeverWork)
             {
                 TryActivateLever();
-                
             }
             else
+            {
                 TriggerLever();
+            }
         }
+
         private void TryActivateLever()
         {
             if (CheckConditions())
             {
                 CanLeverWork = true;
-                FruitManager.Instance.DecreaseFruitNumber(_door.DoorFruitType, _door.DoorFruitNumber);
+
+                // Decrease fruits for all doors (you can adjust logic per door if needed)
+                foreach (var door in _doors)
+                {
+                    if (door != null)
+                        FruitManager.Instance.DecreaseFruitNumber(door.DoorFruitType, door.DoorFruitNumber);
+                }
+
                 TriggerLever();
-                _checkMark.SetActive(true);
-                _leverFruits.SetActive(false);
+
+                if (_checkMark != null)
+                    _checkMark.SetActive(true);
+
+                if (_leverFruits != null)
+                    _leverFruits.SetActive(false);
             }
             else
+            {
                 SoundManager.Instance.PlaySound(7);
+            }
         }
+
         private void TriggerLever()
         {
             SoundManager.Instance.PlaySound(6);
+
             if (IsLeverOn)
                 LeverOff();
             else
                 LeverOn();
-
         }
+
         private void LeverOn()
         {
             IsLeverOn = true;
             _anim.SetBool("IsActive", true);
-            _door.OpenDoor();
+
+            foreach (var door in _doors)
+            {
+                if (door != null)
+                    door.OpenDoor();
+            }
         }
+
         private void LeverOff()
         {
             IsLeverOn = false;
             _anim.SetBool("IsActive", false);
-            _door.CloseDoor();
+
+            foreach (var door in _doors)
+            {
+                if (door != null)
+                    door.CloseDoor();
+            }
         }
+
         private bool CheckConditions()
         {
-            return FruitManager.Instance.AreThereEnoughFruit(_door.DoorFruitType, _door.DoorFruitNumber);
+            // Ensure all doors have their fruit requirements met
+            foreach (var door in _doors)
+            {
+                if (door != null && !FruitManager.Instance.AreThereEnoughFruit(door.DoorFruitType, door.DoorFruitNumber))
+                    return false;
+            }
+            return true;
         }
     }
-
 }
